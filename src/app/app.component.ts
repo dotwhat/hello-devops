980b4259c064b93b6e3a45dd30f7edb7ce001296
Dotwhat <melissa@deverge.nl>
2019-10-06 15:24:59 +0200

    Setup for angular app as azure devops extension

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as SDK from 'azure-devops-extension-sdk';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hello-devops';
+
+  ngOnInit(): void {
+    SDK.init();
+    SDK.ready().then(() => {
+      // Call SDK functions to retrieve information about current user / project etcetera
+    });
+  }
 }
